fa86b9dece9a3a17116bca38e5757225b1b923f5
thiago-papim <thiago.fourp@gmail.com>
2026-03-02 14:02:39 -0300

    salvando original

--- src/utils/ArrProjetos.js
+++ src/utils/ArrProjetos.js
@@ -1,56 +1,67 @@
 /* eslint-disable max-len */
 export const arrBackend = [
   {
-    name: 'Trybe Futebol Clube',
-    url: 'https://github.com/thiago-papim/project-trybe-futebol-clube',
-    description: 'O Trybe Futebol Clube (TFC) é um portal de futebol com recursos de gerenciamento de partidas, classificações detalhadas e login seguro, desenvolvido com tecnologias avançadas como TypeScript, Sequelize, MySQL e Node.js.',
+    name: "Trybe Futebol Clube",
+    url: "https://github.com/thiago-papim/project-trybe-futebol-clube",
+    description:
+      "O Trybe Futebol Clube (TFC) é um portal de futebol com recursos de gerenciamento de partidas, classificações detalhadas e login seguro, desenvolvido com tecnologias avançadas como TypeScript, Sequelize, MySQL e Node.js.",
   },
   {
-    name: 'Blogs API',
-    url: 'https://github.com/thiago-papim/project-blogs-api',
-    description: 'O Blogs API é um avançado sistema de gerenciamento de posts e usuários! 📖 Nele, os usuários podem realizar diversas ações, como criar, ler, atualizar e deletar posts, tudo isso com a segurança de um sistema de login, onde somente o usuário que criou o post tem permissão para deletá-lo ou atualizá-lo.',
+    name: "Blogs API",
+    url: "https://github.com/thiago-papim/project-blogs-api",
+    description:
+      "O Blogs API é um avançado sistema de gerenciamento de posts e usuários! 📖 Nele, os usuários podem realizar diversas ações, como criar, ler, atualizar e deletar posts, tudo isso com a segurança de um sistema de login, onde somente o usuário que criou o post tem permissão para deletá-lo ou atualizá-lo.",
   },
   {
-    name: 'Store Manager',
-    url: 'https://github.com/thiago-papim/project-store-manager',
-    description: 'Este projeto consiste no desenvolvimento de uma API RESTful de gerenciamento de vendas, com foco na criação, visualização, atualização e exclusão de produtos e vendas. A principal tecnologia utilizada é o banco de dados relacional MySQL para armazenar e gerenciar os dados.',
+    name: "Store Manager",
+    url: "https://github.com/thiago-papim/project-store-manager",
+    description:
+      "Este projeto consiste no desenvolvimento de uma API RESTful de gerenciamento de vendas, com foco na criação, visualização, atualização e exclusão de produtos e vendas. A principal tecnologia utilizada é o banco de dados relacional MySQL para armazenar e gerenciar os dados.",
+  },
+
+  {
+    name: "Trybe Smith",
+    url: "https://github.com/thiago-papim/tribesmith",
+    description:
+      "Este projeto visa desenvolver uma API para gerenciar itens medievais, seguindo o padrão CRUD (Create, Read, Update e Delete). A aplicação está escrita em TypeScript e utilizará o Sequelize, um ORM (Object-Relational Mapping), para interagir com um banco de dados.",
   },
   {
-    name: 'Trybe Smith',
-    url: 'https://github.com/thiago-papim/tribesmith',
-    description: 'Este projeto visa desenvolver uma API para gerenciar itens medievais, seguindo o padrão CRUD (Create, Read, Update e Delete). A aplicação está escrita em TypeScript e utilizará o Sequelize, um ORM (Object-Relational Mapping), para interagir com um banco de dados.',
+    name: "MySQL All for One",
+    url: "https://github.com/thiago-papim/mysql-all-for-one",
+    description:
+      'O "MySQL All for One" é um projeto incrível do meu portfólio, consistindo em 27 desafios de consultas SQL desenvolvidos para aprimorar minhas habilidades em manipulação de dados no banco de dados MySQL.',
   },
   {
-    name: 'MySQL All for One',
-    url: 'https://github.com/thiago-papim/mysql-all-for-one',
-    description: 'O "MySQL All for One" é um projeto incrível do meu portfólio, consistindo em 27 desafios de consultas SQL desenvolvidos para aprimorar minhas habilidades em manipulação de dados no banco de dados MySQL.',
-  },
-  {
-    name: 'MySQL One for All',
-    url: 'https://github.com/thiago-papim/mysql-one-for-all',
-    description: 'O projeto One For All é um projeto de normalização e consolidação de dados que visa revisar e aplicar os principais conceitos de banco de dados aprendidos até o momento. Neste projeto, eu me propus a receber planilhas não normalizadas contendo informações diversas e transformá-las em um banco de dados relacional bem estruturado.',
+    name: "MySQL One for All",
+    url: "https://github.com/thiago-papim/mysql-one-for-all",
+    description:
+      "O projeto One For All é um projeto de normalização e consolidação de dados que visa revisar e aplicar os principais conceitos de banco de dados aprendidos até o momento. Neste projeto, eu me propus a receber planilhas não normalizadas contendo informações diversas e transformá-las em um banco de dados relacional bem estruturado.",
   },
 ];
 
 export const arrFrontend = [
   {
-    name: 'Trybe Tunes',
-    url: 'https://github.com/thiago-papim/project-trybetunes',
-    description: 'Desenvolvi o TrybeTunes, uma aplicação completa que oferece uma experiência interativa para os amantes de música. A aplicação possui funcionalidades como login seguro, pesquisa por bandas ou artistas, listagem de álbuns, visualização de músicas, reprodução de prévias, favoritar/desfavoritar músicas, gestão de lista de músicas favoritas, visualização de perfil e edição de perfil para usuários logados.',
+    name: "Trybe Tunes",
+    url: "https://github.com/thiago-papim/project-trybetunes",
+    description:
+      "Desenvolvi o TrybeTunes, uma aplicação completa que oferece uma experiência interativa para os amantes de música. A aplicação possui funcionalidades como login seguro, pesquisa por bandas ou artistas, listagem de álbuns, visualização de músicas, reprodução de prévias, favoritar/desfavoritar músicas, gestão de lista de músicas favoritas, visualização de perfil e edição de perfil para usuários logados.",
   },
   {
-    name: 'Online Store',
-    url: 'https://github.com/thiago-papim/online-store',
-    description: 'Participei do desenvolvimento de uma loja online simplificada em grupo, seguindo demandas definidas em um quadro Kanban, simulando um ambiente próximo ao mercado de trabalho. A aplicação permitirá que os usuários busquem produtos via API do Mercado Livre, interajam com o carrinho de compras, visualizem detalhes e avaliações de produtos, além de simular a finalização da compra. O projeto envolve a compreensão e aplicação de métodos ágeis, Kanban e Scrum, proporcionando uma oportunidade para praticar habilidades do módulo de Front-end.',
+    name: "Online Store",
+    url: "https://github.com/thiago-papim/online-store",
+    description:
+      "Participei do desenvolvimento de uma loja online simplificada em grupo, seguindo demandas definidas em um quadro Kanban, simulando um ambiente próximo ao mercado de trabalho. A aplicação permitirá que os usuários busquem produtos via API do Mercado Livre, interajam com o carrinho de compras, visualizem detalhes e avaliações de produtos, além de simular a finalização da compra. O projeto envolve a compreensão e aplicação de métodos ágeis, Kanban e Scrum, proporcionando uma oportunidade para praticar habilidades do módulo de Front-end.",
   },
   {
-    name: 'Trivia',
-    url: 'https://github.com/thiago-papim/trivia',
-    description: 'Participei no desenvolvimento do Trivia Challenge, um jogo de perguntas e respostas baseado no Trivia, usando React e Redux. Trabalhamos em grupo, organizados por um quadro Kanban no Slack, simulando um ambiente colaborativo de trabalho. A aplicação permite aos usuários logar, associar fotos do Gravatar aos perfis, responder a perguntas com tempo limitado, redirecionar para uma tela de pontuação após 5 respostas, visualizar um ranking, e configurar opções do jogo. Utilizamos React para a interface, Redux para o gerenciamento do estado, e o quadro Kanban no Slack para organização e acompanhamento. O resultado é um jogo envolvente, desafiador e personalizável, promovendo a experiência colaborativa no desenvolvimento.',
+    name: "Trivia",
+    url: "https://github.com/thiago-papim/trivia",
+    description:
+      "Participei no desenvolvimento do Trivia Challenge, um jogo de perguntas e respostas baseado no Trivia, usando React e Redux. Trabalhamos em grupo, organizados por um quadro Kanban no Slack, simulando um ambiente colaborativo de trabalho. A aplicação permite aos usuários logar, associar fotos do Gravatar aos perfis, responder a perguntas com tempo limitado, redirecionar para uma tela de pontuação após 5 respostas, visualizar um ranking, e configurar opções do jogo. Utilizamos React para a interface, Redux para o gerenciamento do estado, e o quadro Kanban no Slack para organização e acompanhamento. O resultado é um jogo envolvente, desafiador e personalizável, promovendo a experiência colaborativa no desenvolvimento.",
   },
   {
-    name: 'Recipes App',
-    url: 'https://github.com/thiago-papim/recipes-app',
-    description: 'Desenvolvi um aplicativo de receitas utilizando tecnologias modernas no ecossistema React, como Hooks e Context API. O app permite visualizar, buscar, filtrar, favoritar e acompanhar o progresso de preparação de receitas de comidas e bebidas. Utilizarei duas APIs distintas para as bases de dados de comidas e bebidas, com foco no layout para dispositivos móveis. Durante o projeto, aplicarei habilidades como Redux para gerenciamento de estado, React-Redux, Context API, React Hooks como useState, useContext e useEffect, além de criar Hooks customizados. O objetivo é proporcionar uma experiência completa e eficiente para os usuários na exploração e preparo de receitas.',
+    name: "Recipes App",
+    url: "https://github.com/thiago-papim/recipes-app",
+    description:
+      "Desenvolvi um aplicativo de receitas utilizando tecnologias modernas no ecossistema React, como Hooks e Context API. O app permite visualizar, buscar, filtrar, favoritar e acompanhar o progresso de preparação de receitas de comidas e bebidas. Utilizarei duas APIs distintas para as bases de dados de comidas e bebidas, com foco no layout para dispositivos móveis. Durante o projeto, aplicarei habilidades como Redux para gerenciamento de estado, React-Redux, Context API, React Hooks como useState, useContext e useEffect, além de criar Hooks customizados. O objetivo é proporcionar uma experiência completa e eficiente para os usuários na exploração e preparo de receitas.",
   },
 ];
